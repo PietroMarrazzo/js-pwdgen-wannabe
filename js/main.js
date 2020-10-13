f566d32cb72493f11a91f37bf72ee1d0a61b4d77
Pietro <marrazzo.pietroalfonso@gmail.com>
2020-10-13 13:33:59 +0200

    first push

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// alert
+alert("Ciao! Inserisci le tue generali nei forms sottostanti");
+
+// nome
+var nome = prompt(Nome);
+// cognome
+var cognome = prompt(Cognome);
+// colore
+var colore = prompt(Colore);
+// visualizzazione
+document.getElementById("generali").innerHTML="nome" +" " + "cognome" + " " + "19";
